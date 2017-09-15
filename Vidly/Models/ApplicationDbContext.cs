66b15d87fa63b33913627d247054b3e971ac4793
sergiolondono <sergio.londonog@gmail.com>
2017-09-15 08:36:25 -0500

    20170915. Separate Class for AccountViewModel

--- Vidly/Models/ApplicationDbContext.cs
+++ Vidly/Models/ApplicationDbContext.cs
@@ -6,14 +6,6 @@ using System.Threading.Tasks;
 
 namespace Vidly.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        [Required]
-        [StringLength(255)]
-        public string DrivingLicense { get; set; }
-    }
-
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Customer> Customers { get; set; }
---
+++ Vidly/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(255)]
+        public string DrivingLicense { get; set; }
+    }
+}
