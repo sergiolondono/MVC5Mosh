56334fb676e4e5ad24a6347e00fad59d7ce9c9dc
sergiolondono <sergio.londonog@gmail.com>
2017-09-19 14:20:43 -0500

    20170919. Cache HTML

---
+++ Vidly/Models/IdentityModels_2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Vidly.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    //public class ApplicationUser : IdentityUser
+    //{
+    //    [Required]
+    //    [StringLength(255)]
+    //    public string DrivingLicense { get; set; }
+    //}
+
+    //public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    //{
+    //    public DbSet<Customer> Customers { get; set; }
+    //    public DbSet<Movie> Movies { get; set; }
+    //    public DbSet<MembershipType> MembershipTypes { get; set; }
+    //    public DbSet<Genre> Genres { get; set; }
+
+    //    public ApplicationDbContext()
+    //        : base("DefaultConnection")
+    //    {
+    //    }
+
+    //    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    //    {
+    //        Database.SetInitializer<ApplicationDbContext>(null);
+    //        base.OnModelCreating(modelBuilder);
+    //    }
+    //}
+}
